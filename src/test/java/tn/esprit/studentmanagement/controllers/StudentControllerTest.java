41c6dc56191578c1e80928aad34f9571e505267f
SoraExplora <34798074+SoraExplora@users.noreply.github.com>
2025-11-28 17:50:25 +0100

    Update StudentControllerTest.java

--- src/test/java/tn/esprit/studentmanagement/controllers/StudentControllerTest.java
+++ src/test/java/tn/esprit/studentmanagement/controllers/StudentControllerTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(StudentController.class)
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -28,7 +33,7 @@ public class StudentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+   @Mock
     private IStudentService studentService;
 
     private Student student1;
