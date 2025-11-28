aeff0374f791db8140550d5b02fea5aaa96eab8e
SoraExplora <34798074+SoraExplora@users.noreply.github.com>
2025-11-28 17:26:37 +0100

    Update StudentRepository.java

--- src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
+++ src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
@@ -6,4 +6,6 @@ import tn.esprit.studentmanagement.entities.Course;
 import tn.esprit.studentmanagement.entities.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+    List<Student> findByDepartment(String department);
 }
